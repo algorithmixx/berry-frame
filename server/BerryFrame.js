61b52e1250c29f4187de38501608adf48ca77e8c
Gero Scholz <gero.scholz@gmail.com>
2020-01-12 21:31:25 +0100

    readme updated

--- server/BerryFrame.js
+++ server/BerryFrame.js
@@ -27,7 +27,7 @@ class BerryFrame {
 		// get current script name
 
 		this.scriptName	 = process.argv[1].replace(/.*[/\\]/,'').replace(/[.]js$/,'');
-		this.versionId	 = "1.3.0";
+		this.versionId	 = "1.3.1";
 		
 		// find known Berry types and their default properties (description, port, rev, ..)
 		this.berryTypes = this.findBerryTypes();
